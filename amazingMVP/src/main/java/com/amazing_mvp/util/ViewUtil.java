345487915adb4a4329d43f9a973d9b55f743fe6f
ppamorim <pp.amorim@hotmail.com>
2015-04-20 16:29:51 -0300

    Copyright added

--- amazingMVP/src/main/java/com/amazing_mvp/util/ViewUtil.java
+++ amazingMVP/src/main/java/com/amazing_mvp/util/ViewUtil.java
@@ -1,13 +1,25 @@
+/*
+* Copyright (C) 2015 Pedro Paulo de Amorim
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.amazing_mvp.util;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Display;
-import android.view.WindowManager;
 import android.widget.TextView;
 import com.amazing_mvp.core.view.fresco.InstrumentedDraweeView;
 import com.amazing_mvp.di.scopes.ActivityScope;
@@ -15,7 +27,6 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
-import javax.inject.Inject;
 
 public class ViewUtil {
 
